44c3855bd6d39d2755eb44e0201ce5fd26fb39bc
Mansamusa007 <alejandrocan@umass.edu>
2020-11-04 20:30:42 -0500

    my first package

---
+++ R/AdvanceTest2.R
@@ -0,0 +1,8 @@
+Rrocks <- function() {
+  print("If a Political Science Major can create an R Package, so can you!")
+}
+library(devtools)
+
+
+library(Test2)
+
---
+++ R/hello.R
@@ -0,0 +1,24 @@
+# Hello, world!
+#
+# This is an example function named 'hello'
+# which prints 'Hello, world!'.
+#
+# You can learn more about package authoring with RStudio at:
+#
+#   http://r-pkgs.had.co.nz/
+#
+# Some useful keyboard shortcuts for package authoring:
+#
+#   Install Package:           'Cmd + Shift + B'
+#   Check Package:             'Cmd + Shift + E'
+#   Test Package:              'Cmd + Shift + T'
+
+
+Rrocks <- function() {
+  print("If a Political Science Major can create an R Package, so can you!")
+}
+
+library(devtools)
+Rrocks()
+
+
